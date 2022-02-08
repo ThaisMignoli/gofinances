a125b4f9f71d0660173f1a5aae1a1f243297ed49
ThaisMignoli <thais.r.mignoli@gmail.com>
2022-02-08 18:57:10 -0300

    add category select modal

---
+++ src/screens/CategorySelect/styles.ts
@@ -0,0 +1,62 @@
+import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from '@expo/vector-icons';
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export const Container = styled(GestureHandlerRootView)`
+  flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  width: 100%;
+  height: ${RFValue(113)}px;
+
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 19px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+`;
+
+export const Category = styled.View`
+  width: 100%;
+  padding: ${RFValue(15)}px;
+
+  flex-direction: row;
+  align-items: center;
+`; 
+
+export const Icon = styled(Feather)`
+  color: ${({ theme }) => theme.colors.text};
+
+  font-size: ${RFValue(20)}px;
+  margin-right: 16px;
+`;
+
+export const Name = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+`;
+
+export const Separator = styled.View`
+  background-color: ${({ theme }) => theme.colors.text};
+
+  height: 1px;
+  width: 100%;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  padding: 24px;
+`;
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
+import { Modal } from "react-native";
 
 import { Button } from "../../components/Form/Button";
-import { CategorySelect } from "../../components/Form/CategorySelect";
+import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
 import { Input } from "../../components/Form/Input";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+
+import { CategorySelect } from "../CategorySelect";
 
 import { 
   Container,
@@ -16,9 +19,23 @@ import {
 
 export function Register() {
   const [transactionType, setTransactionType] = useState('');
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
 
-  function HandleTransactionTypeSelect(type: 'up' | 'down') {
+  const [category, setCategory] = useState({
+    key: 'category',
+    name: 'Categoria'
+  })
+  
+  function handleTransactionTypeSelect(type: 'up' | 'down') {
     setTransactionType(type);
+  }
+
+  function handleOpenSelectCategoryModal() {
+    setCategoryModalOpen(true);
+  }
+
+  function handleCloseSelectCategoryModal() {
+    setCategoryModalOpen(false);
   }
 
   return (
@@ -40,22 +57,36 @@ export function Register() {
               <TransactionTypeButton 
                 title="Income"
                 type="up"
-                onPress={() => HandleTransactionTypeSelect('up')}
+                onPress={() => handleTransactionTypeSelect('up')}
                 isActive={transactionType === 'up'}
               />
               <TransactionTypeButton 
                 title="Outcome"
                 type="down"
-                onPress={() => HandleTransactionTypeSelect('down')}
+                onPress={() => handleTransactionTypeSelect('down')}
                 isActive={transactionType === 'down'}
               />
             </TransactionTypes>
 
-            <CategorySelect title="Categoria"/>
+            <CategorySelectButton 
+              title="Categoria"
+              onPress={handleOpenSelectCategoryModal}
+            />
           </Fields>
 
           <Button title="Enviar" />
         </Form>
+
+        <Modal 
+          visible={categoryModalOpen}
+          statusBarTranslucent={true}
+        >
+          <CategorySelect 
+              category={category}
+              setCategory={setCategory}
+              closeSelectCategory={handleCloseSelectCategoryModal}
+          />
+        </Modal>
 
       </Container>
   );
